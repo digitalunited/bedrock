2bc185adb3e5db0a7ebefe613870040132b44baa
Lucas <lucas@careofhaus.se>
2015-09-28 16:20:49 +0200

    updated the scripts

--- scripts/Roots/Bedrock/plugin_activate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-wp plugin activate js_composer
-wp plugin activate disable-comments
-wp plugin activate filenames-to-latin
-wp plugin activate soil
-wp plugin activate vc-clean-up
-wp plugin activate wordpress-clean-up
-wp plugin activate cms-tree-page-view
