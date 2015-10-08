47d520a60760e2dd8a410c73bc3125025855534d
Lucas Andersson <lucas@careofhaus.se>
2015-10-08 17:26:57 +0200

    move theme folder

--- scripts/Roots/Bedrock/install_theme.sh
+++ scripts/Roots/Bedrock/install_theme.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 cd web/app/themes
 
-curl -Lo default.zip 'http://github.com/digitalunited/sage/archive/master.zip'
-unzip default.zip
-
-rm default.zip
-mv sage-master default
+mv care-of-haus-premium-starter-theme careofhaus
 cd default
