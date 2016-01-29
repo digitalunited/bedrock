e0cf4e6bb94b1194efce49aec843e094d5d4d230
Lucas Andersson <lucas@careofhaus.se>
2016-01-29 13:33:53 +0100

    Fixes map issue

--- scripts/Roots/Bedrock/npm_install.sh
+++ scripts/Roots/Bedrock/npm_install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd web/app/themes
-git clone git@bitbucket.org:careofhaus/care-of-haus-premium-starter-child-theme.git
+git clone git@bitbucket.org:careofhaus/care-of-haus-premium-starter-child-theme.git careofhaus-child
 cd careofhaus-child
 npm install && bower install && gulp
