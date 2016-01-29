2ef5d54ddc0961dc5d15a5ac3881e8876d50f23a
Lucas Andersson <lucas@careofhaus.se>
2016-01-29 13:28:47 +0100

    Smoother startup

--- scripts/Roots/Bedrock/install_theme.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd web/app/themes
-
-mv care-of-haus-premium-starter-theme careofhaus
-cd careofhaus
--- scripts/Roots/Bedrock/npm_install.sh
+++ scripts/Roots/Bedrock/npm_install.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd web/app/themes/default
+cd web/app/themes
+git clone git@bitbucket.org:careofhaus/care-of-haus-premium-starter-child-theme.git
+cd careofhaus-child
 npm install && bower install && gulp
