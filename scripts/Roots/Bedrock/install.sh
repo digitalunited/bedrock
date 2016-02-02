ef002148275132de5bf2d106b2d574aad40f60d3
Lucas Andersson <lucas@careofhaus.se>
2016-02-02 08:52:17 +0100

    Even better install script.

--- scripts/Roots/Bedrock/install.sh
+++ scripts/Roots/Bedrock/install.sh
@@ -3,3 +3,4 @@ cd web/app/themes
 git clone git@bitbucket.org:careofhaus/care-of-haus-premium-starter-child-theme.git careofhaus-child
 cd careofhaus-child
 npm install && bower install && gulp
+rm -rf .git
