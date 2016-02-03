e38df5a3fe9016c36a4019f0b471d762069e313a
Lucas Andersson <lucas@careofhaus.se>
2016-02-03 10:20:05 +0100

    support for our new theme

--- scripts/Roots/Bedrock/install.sh
+++ scripts/Roots/Bedrock/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd web/app/themes
-git clone git@bitbucket.org:careofhaus/care-of-haus-premium-starter-child-theme.git careofhaus-child
-cd careofhaus-child
+git clone git@bitbucket.org:careofhaus/care-of-haus-wordpress-theme.git careofhaus
+cd careofhaus
 npm install && bower install && gulp
 rm -rf .git
