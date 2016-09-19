aad4f33b8d74ff3accc960dc16cd203631264536
Lucas Andersson <lucas@careofhaus.se>
2016-09-19 15:30:09 +0200

    update setup script

--- setup.sh
+++ setup.sh
@@ -16,11 +16,12 @@ open http://$envURL
 read -p "Klicka på [Enter] när WordPress är installerat"
 
 wp theme activate careofhaus
-wp plugin activate --all
+wp plugin activate soil advanced-custom-fields-pro cms-tree-page-view disable-comments filenames-to-latin gravityforms post-duplicator vc-clean-up wordpress-clean-up visual-composer
 wp rewrite structure '/%postname%/'
 wp post create --post_type=page --post_title='Startsida' --post_status=publish
-wp post delete 2
-wp post delete 1
+wp post delete 2 --force
+wp post delete 1 --force
 wp comment delete 1 --force
 wp option update page_on_front 3
 wp option update show_on_front page
+wp option update blogdescription ''
