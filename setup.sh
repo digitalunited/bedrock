b8290420ec229671387b6e46ef086feb56024a2b
Lucas Andersson <lucas@careofhaus.se>
2016-09-19 15:45:26 +0200

    minor change to setup

--- setup.sh
+++ setup.sh
@@ -17,7 +17,7 @@ read -p "Klicka på [Enter] när WordPress är installerat"
 
 wp theme activate careofhaus
 wp plugin activate soil advanced-custom-fields-pro cms-tree-page-view disable-comments filenames-to-latin gravityforms post-duplicator vc-clean-up wordpress-clean-up visual-composer
-wp rewrite structure '/%postname%/'
+wp rewrite structure '/%postname%/' --hard
 wp post create --post_type=page --post_title='Startsida' --post_status=publish
 wp post delete 2 --force
 wp post delete 1 --force
@@ -25,3 +25,4 @@ wp comment delete 1 --force
 wp option update page_on_front 3
 wp option update show_on_front page
 wp option update blogdescription ''
+wp cache flush
