2375dbbd7a189c537b9ea511ae183a656a7f1f24
Lucas <lucas@careofhaus.se>
2017-04-24 15:38:29 +0200

    add user role plugin and better setup script

--- setup.sh
+++ setup.sh
@@ -16,13 +16,13 @@ open http://$envURL
 read -p "Klicka på [Enter] när WordPress är installerat (du behöver inte logga in)"
 
 wp theme activate careofhaus
-wp plugin activate soil advanced-custom-fields-pro cms-tree-page-view disable-comments filenames-to-latin post-duplicator vc-clean-up wordpress-clean-up js_composer
+wp plugin activate soil advanced-custom-fields-pro advanced-custom-fields-nav-menu-field cms-tree-page-view disable-comments filenames-to-latin post-duplicator vc-clean-up wordpress-clean-up js_composer
 wp rewrite structure '/%postname%/' --hard
 wp post create --post_type=page --post_title='Startsida' --post_status=publish
 wp post delete 2 --force
 wp post delete 1 --force
 wp comment delete 1 --force
-wp option update page_on_front 3
+wp option update page_on_front 4
 wp option update show_on_front page
 wp option update blogdescription ''
 wp cache flush
