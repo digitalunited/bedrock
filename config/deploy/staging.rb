5e0653bd1b6eca6abcfd014580fa1cc9b07d414e
Tim Samuelsson <tim.samuelsson@gmail.com>
2017-11-22 12:33:06 +0100

    Added hooks for deactivating/activation plugins when pulling/pushing DB.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -4,26 +4,8 @@ server fetch(:staging_server), user: 'deploy', roles: %w{web app db}
 set :wpcli_remote_url, fetch(:staging_url)
 fetch(:default_env).merge!(wp_env: :staging)
 
-namespace :plugin do
-    desc 'Enable/disabe plugins'
-    task :staging_push do
-        on roles(:app) do
-            within release_path do
-                execute :wp, :plugin, :activate, fetch(:staging_plugins)
-            end
-        end
-    end
-end
+set :plugins, fetch(:staging_plugins)
+after 'wpcli:db:push', 'plugin:db_push'
 
-after 'wpcli:db:push', 'plugin:staging_push'
-
-namespace :plugin do
-    desc 'Enable/disabe plugins'
-    task :staging_pull do
-        run_locally do
-            execute :wp, :plugin, :deactivate, fetch(:staging_plugins)
-        end
-    end
-end
-
-after 'wpcli:db:pull', 'plugin:staging_pull'
+set:plugins, fetch(:staging_plugins)
+after 'wpcli:db:pull', 'plugin:db_pull'
