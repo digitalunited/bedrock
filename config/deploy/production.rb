9e3e5a4e455b1db91304ffdbdc7ea7b951a3f69b
Tim Samuelsson <tim.samuelsson@gmail.com>
2017-11-22 12:32:28 +0100

    Added hooks for deactivating/activation plugins when pulling/pushing DB.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,26 +4,8 @@ server fetch(:production_server), user: 'deploy', roles: %w{web app db}
 set :wpcli_remote_url, fetch(:production_url)
 fetch(:default_env).merge!(wp_env: :production)
 
-namespace :plugin do
-    desc 'Enable/disabe plugins on push'
-    task :production_push do
-        on roles(:app) do
-            within release_path do
-                execute :wp, :plugin, :activate, fetch(:production_plugins)
-            end
-        end
-    end
-end
+set :plugins, fetch(:production_plugins)
+after 'wpcli:db:push', 'plugin:db_push'
 
-after 'wpcli:db:push', 'plugin:production_push'
-
-namespace :plugin do
-    desc 'Enable/disabe plugins on pull'
-    task :production_pull do
-        run_locally do
-            execute :wp, :plugin, :deactivate, fetch(:production_plugins)
-        end
-    end
-end
-
-after 'wpcli:db:pull', 'plugin:production_pull'
+set:plugins, fetch(:production_plugins)
+after 'wpcli:db:pull', 'plugin:db_pull'
