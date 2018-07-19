89d1f2cab1e3809670cac50ae144ab10f764ea81
Kim Eriksson <kim.eriksson@careofhaus.se>
2018-07-19 14:40:31 +0200

    Added Mailtrap, Google Maps API key & max revisions.

--- config/environments/production.php
+++ config/environments/production.php
@@ -13,3 +13,5 @@ define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
 define('DISALLOW_FILE_MODS', true); // this disables all file modifications including updates and update notifications
 define('FS_METHOD', 'direct');
+
+define( 'WP_POST_REVISIONS', 10 );
