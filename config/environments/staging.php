83ffed77b50a5a80aeceec52b160c670353ba3c2
Kim Eriksson <kim.eriksson@careofhaus.se>
2018-07-23 10:42:34 +0200

    Added Mailtrap for production and staging as well.

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -8,6 +8,10 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
+define('MAILTRAP', getenv('MAILTRAP'));
+define('MAILTRAP_USERNAME', getenv('MAILTRAP_USERNAME'));
+define('MAILTRAP_PASSWORD', getenv('MAILTRAP_PASSWORD'));
+
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
